ed5dc3dc8a0c2afc3c06f9ae0c1f328cb1a3ea74
coolqiyu <coolqiyu@gmail.com>
2018-01-23 23:21:38 +0800

    add interacation between parent and child

--- exercise/react/react-app/src/game.js
+++ exercise/react/react-app/src/game.js
@@ -3,18 +3,50 @@ import ReactDOM from 'react-dom';
 import './game.css';
 
 class Square extends React.Component{
+    constructor(){
+        super();
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {value:null}
+    }
+    handleClick(){
+        this.setState({value:'X'});
+    }
     render(){
         return (
-            <button className = "square">
-            {/*TODO*/}
+            // 可以使用箭头函数来解决this的问题
+            // <button className = "square" onClick={()=>this.handleClick()}>
+            // {this.state.value}
+            // </button>
+
+            //子组件被触发onClick事件时，直接是调用父组件通过props传过来的onClick函数
+            <button className="square" onClick={()=>this.props.onClick()}>
+            {this.props.value}
             </button>
         )
     }
 }
-
+//Board中包括多个square
 class Board extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            Squares: Array(9).fill(null)
+        }
+    }
+    handleClick(i){
+        const squares = this.state.Squares.slice();
+        squares[i] = 'X';
+        this.setState({Squares: squares});
+    }
     renderSquare(i){
-        return <Square />;
+        // 给Square传一个value属性，这里把父组件的state设置为子组件的属性
+        // 增加一个onClick属性，对应的方法是当前父组件的方法
+        return (
+            <Square 
+                value={this.state.Squares[i]} 
+                onClick = {() => this.handleClick(i)}
+            />
+        );
     }
     render(){
         const status = "Nex player X";
@@ -56,5 +88,4 @@ class Game extends React.Component{
         )
     }
 }
-ReactDOM.render(<Game />, document.getElementById('game'));
 export default Game;
--- exercise/react/react-app/src/index.js
+++ exercise/react/react-app/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Game from './game';//需要引入game，才会执行，但这个引入并没有什么用处
+import Game from './game';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('game'));
 registerServiceWorker();
